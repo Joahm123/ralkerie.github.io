39635bdbe199c97ae51007d3b1b54a7877687f40
Joahm123 <161253605+Joahm123@users.noreply.github.com>
2026-08-13 22:33:26 -0500

    Add comments and console log to main.js
    Added header comments and initial log statement.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+/* =====================================================
+   MAIN
+===================================================== */
 
+/*
+   This file is intentionally small.
+
+   Put general website logic here later.
+   Keep individual systems in their own files.
+*/
+
+console.log(
+    "Ralkerie website loaded."
+);
