9e98ae3f04eeea09e703b27c02c832fa5b3cd485
Joahm123 <161253605+Joahm123@users.noreply.github.com>
2026-08-15 02:30:12 -0500

    Update waveform.js

--- js/waveform.js
+++ js/waveform.js
@@ -1,64 +1,9 @@
-/* =====================================================
-   RALKERIE WAVEFORM
-===================================================== */
+(function () {
 
-const waveform =
-    document.getElementById(
-        "waveform"
+    "use strict";
+
+    console.log(
+        "Ralkerie waveform disabled."
     );
 
-const BAR_COUNT = 70;
-
-
-/* =====================================================
-   CREATE BARS
-===================================================== */
-
-for (
-    let i = 0;
-    i < BAR_COUNT;
-    i++
-) {
-
-    const bar =
-        document.createElement(
-            "div"
-        );
-
-    bar.className =
-        "wave-bar";
-
-
-    const center =
-        Math.abs(
-            i -
-            BAR_COUNT / 2
-        ) /
-        (BAR_COUNT / 2);
-
-
-    const height =
-        1 -
-        center * 0.65;
-
-
-    bar.style.height =
-        `${20 + height * 45}px`;
-
-
-    bar.style.setProperty(
-        "--speed",
-        `${0.45 + Math.random() * 0.55}s`
-    );
-
-
-    bar.style.setProperty(
-        "--delay",
-        `${Math.random() * -1}s`
-    );
-
-
-    waveform.appendChild(
-        bar
-    );
-}
+})();
